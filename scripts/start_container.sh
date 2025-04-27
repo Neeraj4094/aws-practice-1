ba6126107c9fc4417125d4c82b9407b21ebaae74
Neeraj <123150121+Neeraj4094@users.noreply.github.com>
2025-04-27 13:36:37 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull neeraj4094/simple-python-flask-app
-
+ 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 neeraj4094/simple-python-flask-app
